bbad1dc2f57c66453e362d096e26403ae4a7f18d
vifclem <clement.expert@artfx.fr>
2023-03-24 18:50:58 +0100

    [add] smoke effect and hold button desactivated

--- Assets/Script/Shooting.cs
+++ Assets/Script/Shooting.cs
@@ -1,14 +1,4 @@
 using UnityEngine;
-using TMPro;
-
-/// Thanks for downloading my projectile gun script! :D
-/// Feel free to use it in any project you like!
-/// 
-/// The code is fully commented but if you still have any questions
-/// don't hesitate to write a yt comment
-/// or use the #coding-problems channel of my discord server
-/// 
-/// Dave
 public class Shooting : MonoBehaviour
 {
    
@@ -25,7 +15,7 @@ public class Shooting : MonoBehaviour
     [Header("Gun infos")]
     public float timeBetweenShooting, spread, reloadTime, timeBetweenShots;
     public int magazineSize, bulletsPerTap;
-    public bool allowButtonHold;
+    //public bool allowButtonHold;
 
     int bulletsLeft, bulletsShot;
 
@@ -65,14 +55,12 @@ public class Shooting : MonoBehaviour
     }
     private void MyInput()
     {
-        //Check if allowed to hold down button and take corresponding input
-        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
-        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+       shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
         //Jouer l'anim recoil
         if (Input.GetKey(KeyCode.Mouse0))
         {
             IsShooting = true;
-            //animator.SetTrigger("Shooting");
         }
         
         //Reloading 
@@ -83,7 +71,6 @@ public class Shooting : MonoBehaviour
         //Shooting
         if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
         {
-            //Set bullets shot to 0
             bulletsShot = 0;
 
             Shoot();
@@ -99,8 +86,8 @@ public class Shooting : MonoBehaviour
             animator.SetTrigger("Shooting");
         }
 
-        //Find the exact hit position using a raycast
-        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); //Just a ray through the middle of your current view
+       
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); 
         RaycastHit hit;
 
         //check if ray hits something
@@ -108,50 +95,44 @@ public class Shooting : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
             targetPoint = hit.point;
         else
-            targetPoint = ray.GetPoint(75); //Just a point far away from the player
+            targetPoint = ray.GetPoint(75); 
 
-        //Calculate direction from attackPoint to targetPoint
+        
         Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
 
-        //Calculate spread
+        //Calculer le spread
         float x = Random.Range(-spread, spread);
         float y = Random.Range(-spread, spread);
 
-        //Calculate new direction with spread
-        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0); //Just add spread to last direction
+        //Calculate la nouvelle direction avec le spread
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0); 
 
-        //Instantiate bullet/projectile
-        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity); //store instantiated bullet in currentBullet
-        //Rotate bullet to shoot direction
+        
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity); 
         currentBullet.transform.forward = directionWithSpread.normalized;
 
-        //Add forces to bullet
+      
         currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
         currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
 
-        //Instantiate muzzle flash, if you have one
+        
        if (ShootVFX != null)
             Instantiate(ShootVFX, attackPoint.position, Quaternion.identity);
 
         bulletsLeft--;
         bulletsShot++;
 
-        //Invoke resetShot function (if not already invoked), with your timeBetweenShooting
         if (allowInvoke)
         {
             Invoke("ResetShot", timeBetweenShooting);
             allowInvoke = false;
-
-           
         }
-
-        //if more than one bulletsPerTap make sure to repeat shoot function
         if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
             Invoke("Shoot", timeBetweenShots);
     }
     private void ResetShot()
     {
-        //Allow shooting and invoking again
+        
         readyToShoot = true;
         allowInvoke = true;
     }
@@ -159,13 +140,11 @@ public class Shooting : MonoBehaviour
     private void Reload()
     {
         reloading = true;
-        Invoke("ReloadFinished", reloadTime); //Invoke ReloadFinished function with your reloadTime as delay
+        Invoke("ReloadFinished", reloadTime);
         reloadAudioSource.Play();
-
     }
     private void ReloadFinished()
     {
-        //Fill magazine
         bulletsLeft = magazineSize;
         reloading = false;
     }
