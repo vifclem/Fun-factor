2651877857f60b0d072e79d09d423052d4022a06
vifclem <clement.expert@artfx.fr>
2023-04-11 18:37:56 +0200

    [add] more force and new explosion sound

---
+++ Assets/Script/CubeVFX.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeVFX : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (CompareTag("ground"))
+        {
+
+            Destroy(target, 0.5f);
+        }
+    }
+}
